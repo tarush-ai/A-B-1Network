891b30104e0bcc34c3133175ff413284e2666a3b
TarushGupta12345 <tarushgs@gmail.com>
2026-02-19 19:58:58 -0800

    final submission version

--- AB1.java
+++ AB1.java
@@ -1,23 +1,23 @@
 /*
 * Author: Tarush Gupta
 * Date: February 17, 2026
-* Description: A-B-1 Neural Network with backprop training.
+* Description: A-B-1 Neural Network with gradient descent training.
 *              Supports configurable layers, rand/fixed init, and training/running modes.
 */
-
 public class AB1
 {
 /*
 * Network config
 */
-   public int A;
-   public int B;
+   public int inputLayer;
+   public int hiddenLayer;
    public double lambda;
    public double low;
    public double high;
    public int maxIterations;
    public double errorThreshold;
-   public String truthTable;
+   public boolean useRandomWeights;
+   public int numCases; 
    
 /*
 * Network arrays
@@ -29,11 +29,10 @@ public class AB1
    public double[] wJ0;
    public double[] thetaJ;
    public double theta0;
-   
+   public double[][] inputs;
 /*
 * Training arrays
 */
-   public double[][] inputs;
    public double[] targets;
    public double[][] deltaWKJ;
    public double[] deltaWJ0;
@@ -50,231 +49,301 @@ public class AB1
    public double trainError;
    
    public boolean isTraining;
+   public boolean shouldRun;
    public boolean showInputTable;
    public boolean showTruthTable;
 
-   /*
-    * Sets config for the network. 
-    */
+/*
+* Sets config for the network. 
+*/
    public void setConfig()
    {
-      A = 2;
-      B = 2;
-
+      inputLayer = 2;
+      hiddenLayer = 2;
+      numCases = 4;
       low = -1.5;
       high = 1.5;
       maxIterations = 100000;
       lambda = 0.3;
       errorThreshold = 2e-4;
-      truthTable = "XOR";
+      useRandomWeights = true; 
+      isTraining = true;
+      shouldRun = true;
       showInputTable = true;
       showTruthTable = true;
    } // setConfig()
 
-   /*
-    * Sets training or running mode.
-    */
-   public void setMode(boolean training)
-   {
-      isTraining = training;
-   } // setMode(boolean training)
-
-   /*
-    * Prints current network config.
-    */
+/*
+* Prints current network config.
+*/
    public void echoConfig()
    {
-      System.out.println("Your defined network configuration is:");
-      System.out.println("" + A + "-" + B + "-1");
+      System.out.println("Network Configuration: " + inputLayer + "-" + hiddenLayer + "-1");
+      System.out.println("Number of Test Cases: " + numCases);
+      System.out.println("Weight Initialization: " + (useRandomWeights ? "Random" : "Fixed"));
+      System.out.println("Show Input Table: " + showInputTable);
+      System.out.println("Show Truth Table: " + showTruthTable);
+      System.out.println("Run After Training: " + shouldRun);
+      System.out.println("Mode: " + (isTraining ? "Training" : "Running"));
 
       if (isTraining)
       {
-         System.out.println("Training Configuration:");
-         System.out.println("Random Number Range: " + low + "-" + high);
+         System.out.println("\nTraining Parameters:");
+         System.out.println("Random Number Range: [" + low + ", " + high + "]");
          System.out.println("Max Iterations: " + maxIterations);
          System.out.println("Error Threshold: " + errorThreshold);
-         System.out.println("Lambda value: " + lambda);
+         System.out.println("Lambda: " + lambda);
       }
    } // echoConfig()
 
-   /*
-    * Allocates memory for all network arrays.
-    */
+/*
+* Allocates memory for all network arrays.
+*/
    public void allocateMemory()
    {
-      a = new double[A];
-      h = new double[B];
-      wKJ = new double[A][B];
-      wJ0 = new double[B];
-      thetaJ = new double[B];
+      a = new double[inputLayer];
+      h = new double[hiddenLayer];
+      wKJ = new double[inputLayer][hiddenLayer];
+      wJ0 = new double[hiddenLayer];
+      thetaJ = new double[hiddenLayer];
       
-      int numCases = (int)power(2.0, A);
-      inputs = new double[numCases][A];
+      inputs = new double[numCases][inputLayer];
       results = new double[numCases];
 
       if (isTraining)
       {
          targets = new double[numCases];
-         deltaWKJ = new double[A][B];
-         deltaWJ0 = new double[B];
-         omegaJ = new double[B];
-         psiJ = new double[B];
+         deltaWKJ = new double[inputLayer][hiddenLayer];
+         deltaWJ0 = new double[hiddenLayer];
+         omegaJ = new double[hiddenLayer];
+         psiJ = new double[hiddenLayer];
       }
    } // allocateMemory()
 
-   /*
-    * Populates network arrays.
-    * @param random true for rand init, false for fixed
-    */
-   public void populateArrays(boolean random)
-   {
-      int k;
-      int j;
-
-      if (A == 2)
-      {
-         inputs[0][0] = 0.0; inputs[0][1] = 0.0;
-         inputs[1][0] = 0.0; inputs[1][1] = 1.0;
-         inputs[2][0] = 1.0; inputs[2][1] = 0.0;
-         inputs[3][0] = 1.0; inputs[3][1] = 1.0;
-
-         if (isTraining)
-         {
-            setTruthTable2();
-         }
-      } // if (A == 2)
-      else if (A == 3)
-      {
-         inputs[0][0] = 0.0; inputs[0][1] = 0.0; inputs[0][2] = 0.0;
-         inputs[1][0] = 0.0; inputs[1][1] = 0.0; inputs[1][2] = 1.0;
-         inputs[2][0] = 0.0; inputs[2][1] = 1.0; inputs[2][2] = 0.0;
-         inputs[3][0] = 0.0; inputs[3][1] = 1.0; inputs[3][2] = 1.0;
-         inputs[4][0] = 1.0; inputs[4][1] = 0.0; inputs[4][2] = 0.0;
-         inputs[5][0] = 1.0; inputs[5][1] = 0.0; inputs[5][2] = 1.0;
-         inputs[6][0] = 1.0; inputs[6][1] = 1.0; inputs[6][2] = 0.0;
-         inputs[7][0] = 1.0; inputs[7][1] = 1.0; inputs[7][2] = 1.0;
-
-         if (isTraining)
-         {
-            setTruthTable3();
-         }
-      } // else if (A == 3)
-
 /*
-* Initialize weights
+* Populates network arrays.
 */
-      if (random)
+   public void populateArrays()
+   {
+      setInputTable();
+
+      if (isTraining)
       {
-         for (k = 0; k < A; ++k)
-         {
-            for (j = 0; j < B; ++j)
-            {
-               wKJ[k][j] = randomize(low, high);
-            }
-         }
-         for (j = 0; j < B; ++j)
-         {
-            wJ0[j] = randomize(low, high);
-         }
-      } // if (random)
+         setTruthTable();
+      }
+
+      if (useRandomWeights)
+      {
+         useRandomWeights();
+      } 
       else
       {
+         useFixedWeights();
+      } 
+   } // populateArrays()
+
+/*
+* Sets input values for all test cases.
+*/
+   public void setInputTable()
+   {
+      inputs[0][0] = 0.0; inputs[0][1] = 0.0;
+      inputs[1][0] = 0.0; inputs[1][1] = 1.0;
+      inputs[2][0] = 1.0; inputs[2][1] = 0.0;
+      inputs[3][0] = 1.0; inputs[3][1] = 1.0;
+
+/*
+* XOR_AND (A=3)
+* inputs[0][0] = 0.0; inputs[0][1] = 0.0; inputs[0][2] = 0.0;
+* inputs[1][0] = 0.0; inputs[1][1] = 0.0; inputs[1][2] = 1.0;
+* inputs[2][0] = 0.0; inputs[2][1] = 1.0; inputs[2][2] = 0.0;
+* inputs[3][0] = 0.0; inputs[3][1] = 1.0; inputs[3][2] = 1.0;
+* inputs[4][0] = 1.0; inputs[4][1] = 0.0; inputs[4][2] = 0.0;
+* inputs[5][0] = 1.0; inputs[5][1] = 0.0; inputs[5][2] = 1.0;
+* inputs[6][0] = 1.0; inputs[6][1] = 1.0; inputs[6][2] = 0.0;
+* inputs[7][0] = 1.0; inputs[7][1] = 1.0; inputs[7][2] = 1.0;
+*/
+
+/*
+* Conventional
+* inputs[0][0] = 0.0; inputs[0][1] = 0.0;
+* inputs[1][0] = 0.0; inputs[1][1] = 1.0;
+* inputs[2][0] = 1.0; inputs[2][1] = 0.0;
+* inputs[3][0] = 1.0; inputs[3][1] = 1.0;
+*/
+   } // setInputTable()
+
+/*
+* Prints the input table.
+*/
+   public void printInputTable()
+   {
+      System.out.println("\nInput Table:");
+      for (int c = 0; c < numCases; ++c)
+      {
+         for (int k = 0; k < inputLayer; ++k)
+         {
+            System.out.print(inputs[c][k] + "\t");
+         }
+         System.out.println();
+      }
+   } // printInputTable()
+
+/*
+* Prints the truth table.
+*/
+   public void printTruthTable()
+   {
+      System.out.println("\nTruth Table:");
+      for (int c = 0; c < numCases; ++c)
+      {
+         for (int k = 0; k < inputLayer; ++k)
+         {
+            System.out.print(inputs[c][k] + "\t");
+         }
+         System.out.println("| " + targets[c]);
+      }
+   } // printTruthTable()
+
+/*
+* Random weight initialization
+*/
+   public void useRandomWeights()
+   {
+      for (int k = 0; k < inputLayer; ++k)
+      {
+         for (int j = 0; j < hiddenLayer; ++j)
+         {
+            wKJ[k][j] = randomize(low, high);
+         }
+      }
+      for (int j = 0; j < hiddenLayer; ++j)
+      {
+         wJ0[j] = randomize(low, high);
+      }
+   } // useRandomWeights()
+
 /*
 * Fixed weights for 2-2-1 testing
 */
-         if (A == 2 && B == 2)
-         {
-            wKJ[0][0] = 0.76;
-            wKJ[0][1] = 0.9;
-            wKJ[1][0] = 0.9;
-            wKJ[1][1] = 0.76;
-            wJ0[0] = 0.2;
-            wJ0[1] = 0.2;
-         }
-      } // else
-   } // populateArrays(boolean random)
-
-   /*
-    * Sets the truth table targets for A=2 based on the truthTable config.
-    * Supports OR, AND, and XOR.
-    */
-   public void setTruthTable2()
+   public void useFixedWeights()
    {
-      if (truthTable.equals("OR"))
+/*
+* Fixed weights for a 2-2-1 network, optimized for XOR.
+*/
+      if (inputLayer == 2 && hiddenLayer == 2)
       {
-         targets[0] = 0.0;
-         targets[1] = 1.0;
-         targets[2] = 1.0;
-         targets[3] = 1.0;
+         wKJ[0][0] = 0.76;
+         wKJ[0][1] = 0.9;
+         wKJ[1][0] = 0.9;
+         wKJ[1][1] = 0.76;
+         wJ0[0] = 0.2;
+         wJ0[1] = 0.2;
       }
-      else if (truthTable.equals("AND"))
-      {
-         targets[0] = 0.0;
-         targets[1] = 0.0;
-         targets[2] = 0.0;
-         targets[3] = 1.0;
-      }
-      else if (truthTable.equals("XOR"))
-      {
-         targets[0] = 0.0;
-         targets[1] = 1.0;
-         targets[2] = 1.0;
-         targets[3] = 0.0;
-      }
-   } // setTruthTable2()
+   } // useFixedWeights()
 
-   /*
-    * Sets the truth table targets for A=3 based on the truthTable config.
-    * Supports XOR_AND: a[0] XOR a[1] AND a[2].
-    */
-   public void setTruthTable3()
+/*
+* Sets the truth table targets for A=2 based on the truthTable config.
+* Supports OR, AND, and XOR.
+*/
+   public void setTruthTable()
    {
-      if (truthTable.equals("XOR_AND"))
-      {
-         targets[0] = 0.0;
-         targets[1] = 0.0;
-         targets[2] = 0.0;
-         targets[3] = 1.0;
-         targets[4] = 0.0;
-         targets[5] = 1.0;
-         targets[6] = 0.0;
-         targets[7] = 0.0;
-      }
-   } // setTruthTable3()
+/*
+* XOR
+* targets[0] = 0.0;
+* targets[1] = 1.0;
+* targets[2] = 1.0;
+* targets[3] = 0.0;
+*/
 
-   /*
-    * Returns a random double between low and high.
-    */
+/*
+* AND
+* targets[0] = 0.0;
+* targets[1] = 0.0;
+* targets[2] = 0.0;
+* targets[3] = 1.0;
+*/
+
+/*
+* OR
+*/
+      targets[0] = 0.0;
+      targets[1] = 1.0;
+      targets[2] = 1.0;
+      targets[3] = 1.0;
+
+/*
+*a[0] XOR a[1] AND a[2] (a specific test case for A=3)
+* targets[0] = 0.0; 
+* targets[1] = 0.0; 
+* targets[2] = 0.0; 
+* targets[3] = 1.0; 
+* targets[4] = 0.0; 
+* targets[5] = 1.0; 
+* targets[6] = 0.0; 
+* targets[7] = 0.0;
+*/
+   } // setTruthTable()
+
+/*
+* Returns a random double between low and high.
+*/
    public double randomize(double low, double high)
    {
       return low + Math.random() * (high - low);
    } // randomize(double low, double high)
 
-   /*
-    * Calculates base raised to exp.
-    */
-   public double power(double base, int exp)
+/*
+* Generalizable activation function. User can specify this.
+*/
+   public double activation(double x)
    {
-      double result = 1.0;
-      int n;
-      for (n = 0; n < exp; ++n)
-      {
-         result *= base;
-      }
-      return result;
-   } // power(double base, int exp)
+      return sigmoid(x);
+   }
 
-   /*
-    * Sigmoid activation: 1 / (1 + e^-x).
-    */
+/*
+* Generalizable activation function derivative. User can specify this.
+*/
+   public double activationDerivative(double num)
+   {
+      return sigmoidDerivative(num);
+   }
+
+/*
+* Sigmoid activation: 1 / (1 + e^-x).
+*/
    public double sigmoid(double x)
    {
       return 1.0 / (1.0 + Math.exp(-x));
    } // sigmoid(double x)
 
-   /*
-    * Runs the network once using current input activations.
-    */
+/*
+* Sigmoid activation derivative: sigmoid (1 - sigmoid)
+*/
+   public double sigmoidDerivative(double deriv)
+   {
+      return deriv * (1.0 - deriv);
+   }
+
+/*
+* Linear activation: x
+*/
+   public double linear(double x)
+   {
+      return x;
+   }
+
+/*
+* Linear activation derivative: 1
+*/
+   public double linearDerivative(double deriv)
+   {
+      return 1.0;
+   }
+
+/*
+* Runs the network once using current input activations.
+*/
    public void run()
    {
       int j;
@@ -283,38 +352,39 @@ public class AB1
 /*
 * Hidden layer
 */
-      for (j = 0; j < B; ++j)
+      for (j = 0; j < hiddenLayer; ++j)
       {
          thetaJ[j] = 0.0;
-         for (k = 0; k < A; ++k)
+         for (k = 0; k < inputLayer; ++k)
          {
             thetaJ[j] += a[k] * wKJ[k][j];
          }
-         h[j] = sigmoid(thetaJ[j]);
+         h[j] = activation(thetaJ[j]);
       }
 
 /*
 * Output
 */
       theta0 = 0.0;
-      for (j = 0; j < B; ++j)
+      for (j = 0; j < hiddenLayer; ++j)
       {
          theta0 += h[j] * wJ0[j];
       }
-      F0 = sigmoid(theta0);
+      F0 = activation(theta0);
    } // run()
 
-   /*
-    * Trains the network using steepest descent across all training cases.
-    * Stores final iteration count and error in trainIterations and trainError.
-    */
+
+/*
+* Trains the network using steepest descent across all training cases.
+* Stores final iteration count and error in trainIterations and trainError.
+*/
    public void train()
    {
       int iteration = 0;
       double avgError = 1.0;
       int caseIndex;
-      int k;
       int j;
+      int k;
       double totalError;
 
       while (iteration < maxIterations && avgError >= errorThreshold)
@@ -324,59 +394,54 @@ public class AB1
 /*
 * Reset deltas
 */
-         for (j = 0; j < B; ++j)
+         for (j = 0; j < hiddenLayer; ++j)
          {
             deltaWJ0[j] = 0.0;
-            for (k = 0; k < A; ++k)
+            for (k = 0; k < inputLayer; ++k)
             {
                deltaWKJ[k][j] = 0.0;
             }
          }
 
-         for (caseIndex = 0; caseIndex < inputs.length; ++caseIndex)
+         for (caseIndex = 0; caseIndex < numCases; ++caseIndex)
          {
-            for (k = 0; k < A; ++k)
-            {
-               a[k] = inputs[caseIndex][k];
-            }
-
-            run();
+            populateInputAndRun(caseIndex);
 
             omega0 = targets[caseIndex] - F0;
             totalError += 0.5 * omega0 * omega0;
 
 /*
-* Backprop
+* gradient descent
 */
-            psi0 = omega0 * F0 * (1.0 - F0);
+            psi0 = omega0 * activationDerivative(F0);
 
-            for (j = 0; j < B; ++j)
+            for (j = 0; j < hiddenLayer; ++j)
             {
                deltaWJ0[j] += lambda * psi0 * h[j];
 
                omegaJ[j] = psi0 * wJ0[j];
-               psiJ[j] = omegaJ[j] * h[j] * (1.0 - h[j]);
+               psiJ[j] = omegaJ[j] * activationDerivative(h[j]);
                
-               for (k = 0; k < A; ++k)
+               for (k = 0; k < inputLayer; ++k)
                {
                   deltaWKJ[k][j] += lambda * a[k] * psiJ[j];
                }
-            } // for (j = 0; j < B; ++j)
-         } // for (caseIndex = 0; caseIndex < inputs.length; ++caseIndex)
+            } // for (j = 0; j < hiddenLayer; ++j)
+         } // for (caseIndex = 0; caseIndex < numCases; ++caseIndex)
          
 /*
 * Apply weight changes
 */
-         for (j = 0; j < B; ++j)
+         for (j = 0; j < hiddenLayer; ++j)
          {
             wJ0[j] += deltaWJ0[j];
-            for (k = 0; k < A; ++k)
+            for (k = 0; k < inputLayer; ++k)
             {
                wKJ[k][j] += deltaWKJ[k][j];
             }
          }
 
-         avgError = totalError / (double)inputs.length;
+         avgError = totalError / (double)numCases;
          iteration++;
       } // while (iteration < maxIterations && avgError >= errorThreshold)
 
@@ -384,9 +449,9 @@ public class AB1
       trainError = avgError;
    } // train()
 
-   /*
-    * Reports training exit information: reason, iterations, and final error.
-    */
+/*
+* Reports training exit info: reason, iterations, and final error.
+*/
    public void reportTrainingResults()
    {
       System.out.println("Training Exit Information:");
@@ -402,68 +467,59 @@ public class AB1
       System.out.println("Average Error reached: " + trainError);
    } // reportTrainingResults()
 
-   /*
-    * Loops over all test cases, setting inputs and running the network.
-    * Stores each output in results.
-    */
-   public void runAllCases()
+/*
+* Loops over all test cases, setting inputs and running the network.
+* Stores each output in results. Identical to runTestCases().
+*/
+   public void runTrainingCases()
    {
       int caseIndex;
-      int k;
 
-      for (caseIndex = 0; caseIndex < inputs.length; ++caseIndex)
+      for (caseIndex = 0; caseIndex < numCases; ++caseIndex)
       {
-         for (k = 0; k < A; ++k)
-         {
-            a[k] = inputs[caseIndex][k];
-         }
-
-         run();
+         populateInputAndRun(caseIndex);
          results[caseIndex] = F0;
       }
-   } // runAllCases()
+   } // runTrainingCases()
 
-   /*
-    * Prints input table (optional), truth table (optional), and outputs.
-    * Display controlled by showInputTable and showTruthTable config fields.
-    */
+/*
+* Loops over all test cases, setting inputs and running the network.
+* Stores each output in results. Identical to runTrainingCases().
+*/
+   public void runTestCases()
+   {
+      int caseIndex;
+
+      for (caseIndex = 0; caseIndex < numCases; ++caseIndex)
+      {
+         populateInputAndRun(caseIndex);
+         results[caseIndex] = F0;
+      }
+   } // runTestCases()
+
+/*
+* Sets input activations from the given case and runs the network.
+*/
+   public void populateInputAndRun(int caseIndex)
+   {
+      for (int k = 0; k < inputLayer; ++k)
+      {
+         a[k] = inputs[caseIndex][k];
+      }
+      run();
+   }
+
+/*
+* Reports run results: outputs only.
+*/
    public void reportRunResults()
    {
       int caseIndex;
-      int k;
 
       System.out.println("\nRun Results:");
-
-      if (showInputTable)
+      for (caseIndex = 0; caseIndex < numCases; ++caseIndex)
       {
-         System.out.print("Input");
-         for (k = 0; k < A; ++k)
-         {
-            System.out.print("\t");
-         }
-      }
-
-      if (showTruthTable && isTraining)
-      {
-         System.out.print("Target\t");
-      }
-      System.out.println("Output");
-
-      for (caseIndex = 0; caseIndex < inputs.length; ++caseIndex)
-      {
-         if (showInputTable)
-         {
-            for (k = 0; k < A; ++k)
-            {
-               System.out.print(inputs[caseIndex][k] + "\t");
-            }
-         }
-
-         if (showTruthTable && isTraining)
-         {
-            System.out.print(targets[caseIndex] + "\t");
-         }
          System.out.println(results[caseIndex]);
-      } // for (caseIndex = 0; caseIndex < inputs.length; ++caseIndex)
+      }
    } // reportRunResults()
-}
+} // class AB1
